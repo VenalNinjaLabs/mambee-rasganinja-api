dde9713f6fc8c087f8ba8748034078ca77af2e59
Kelvin Campelo Alves de Sousa <kelvin.campelo@ifpi.edu.br>
2018-01-15 02:05:44 -0300

    updated token middleware

--- src/middleware/token.js
+++ src/middleware/token.js
@@ -1,21 +1,29 @@
-import { verify } from 'jsonwebtoken';
-import { ExceptionFactory, isPublic } from '../util';
-import { EXCEPTION } from '../constants/exceptions';
+import {
+    verify
+} from 'jsonwebtoken';
+import {
+    ExceptionFactory,
+    isPublic
+} from '../util';
+import {
+    EXCEPTION
+} from '../constants/exceptions';
 
 export function tokenFilter(req, res, next) {
-    if(isPublic(req)) {
+    if (isPublic(req)) {
         next();
         return;
     }
     let jwtToken = req.headers['x-auth-token'];
-    if(!jwtToken) {
-        next(new ExceptionFactory(EXCEPTION.UNAUTHENTICATED));
+    if (!jwtToken) {
+        next();
+        /*    next(new ExceptionFactory(EXCEPTION.UNAUTHENTICATED)); */
     }
     verify(jwtToken, process.env.JWT_SECRET, (err, decoded) => {
-        if(err) {
+        if (err) {
             next(new ExceptionFactory(EXCEPTION.UNAUTHENTICATED));
         }
-        if(typeof decoded === 'object') req.token = decoded;
+        if (typeof decoded === 'object') req.token = decoded;
         next();
     })
 }
